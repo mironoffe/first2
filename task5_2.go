51869ea76b94def9823df69096c456c2612a948f
mironov <mironov@indins.ru>
2020-08-26 15:54:59 +0300

    task 5_2

--- task5_2.go
+++ task5_2.go
@@ -1,29 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 //2) Считать  строку.  и вставить после каждого символа [a-z]   (нижнее подчерк)_
 func main() {
 	var str string
 	fmt.Scan(&str)
-
-	//for i := int('a'); i <= int('z'); i++ {
-	//	str = strings.ReplaceAll(str, string(i), string(i)+"_")
-	//}
-	//
-	//result := ""
-	//for _, symbol := range str {
-	//	result += string(symbol)
-	//	if symbol >= 'a' && symbol <= 'z' {
-	//		result += "_"
-	//	}
-	//}
-	//fmt.Println(result)
-
-	strlen := len(str)
-	result := make([]rune, strlen, strlen*2)
+	result := make([]rune, 0)
 	for _, symbol := range str {
 		result = append(result, symbol)
 		if symbol >= 'a' && symbol <= 'z' {
